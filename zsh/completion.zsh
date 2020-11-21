21603a9b1c34f0b13999222a42df0ee2c87c2217
Kenny Baustert <kdbaustert@fullsail.edu>
2020-11-20 22:55:15 -0500

    Create completion.zsh

---
+++ zsh/completion.zsh
@@ -0,0 +1,81 @@
+# @author     Kenny B
+# Github:     https://github.com/kdbaustert/dotfiles
+
+#
+# tab completion configuration
+#
+
+zstyle ':completion:*' completer _expand _complete _ignored _approximate
+zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
+zstyle ':completion:*:descriptions' format '-- %d --'
+zstyle ':completion:*:processes' command 'ps -au$USER'
+zstyle ':completion:complete:*:options' sort false
+zstyle ':fzf-tab:complete:_zlua:*' query-string input
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm,cmd -w -w"
+zstyle ':fzf-tab:complete:kill:argument-rest' extra-opts --preview=$extract'ps --pid=$in[(w)1] -o cmd --no-headers -w -w' --preview-window=down:3:wrap
+zstyle ':fzf-tab:complete:cd:*' extra-opts --preview=$extract'exa -1 --color=always ${~ctxt[hpre]}$in'zstyle ':completion:*' completer _expand _complete _ignored _approximate
+
+# load completions system
+zmodload -i zsh/complist
+
+zstyle ':completion:*' rehash true
+
+# for all completions: menuselection
+zstyle ':completion:*' menu select=1
+
+# for all completions: grouping the output
+zstyle ':completion:*' group-name ''
+
+# for all completions: color
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# for all completions: selected item
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} ma=0\;30
+
+# completion of .. directories
+zstyle ':completion:*' special-dirs true
+
+# fault tolerance
+zstyle ':completion:*' completer _complete _correct _approximate
+# (1 error on 3 characters)
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=( $(( ($#PREFIX+$#SUFFIX)/3 )) numeric )'
+
+# case insensitivity
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+zstyle ":completion:*" matcher-list 'm:{A-Zöäüa-zÖÄÜ}={a-zÖÄÜA-Zöäü}'
+
+# for all completions: grouping / headline / ...
+zstyle ':completion:*:messages' format $'\e[01;35m -- %d -- \e[00;00m'
+zstyle ':completion:*:warnings' format $'\e[01;31m -- No Matches Found -- \e[00;00m'
+zstyle ':completion:*:descriptions' format $'\e[01;33m -- %d -- \e[00;00m'
+zstyle ':completion:*:corrections' format $'\e[01;33m -- %d -- \e[00;00m'
+
+# # statusline for many hits
+zstyle ':completion:*:default' select-prompt $'\e[01;35m -- Match %M    %P -- \e[00;00m'
+
+# for all completions: show comments when present
+zstyle ':completion:*' verbose yes
+
+# in menu selection strg+space to go to subdirectories
+bindkey -M menuselect '^@' accept-and-infer-next-history
+
+# case-insensitive -> partial-word (cs) -> substring completion:
+zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# caching of completion stuff
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "$ZSH_CACHE"
+
+# ~dirs: reorder output sorting: named dirs over userdirs
+zstyle ':completion::*:-tilde-:*:*' group-order named-directories users
+
+# ssh: reorder output sorting: user over hosts
+zstyle ':completion::*:ssh:*:*' tag-order "users hosts"
+
+# kill: advanced kill completion
+zstyle ':completion::*:kill:*:*' command 'ps xf -U $USER -o pid,%cpu,cmd'
+zstyle ':completion::*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;32'
+
+# zstyle :compinstall filename '~/.zshrc'
+
+autoload -Uz compinit && compinit
