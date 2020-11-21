8788e8f1804972ae5a89ebfceb46ae7fb380af4a
Kenny Baustert <kdbaustert@fullsail.edu>
2020-11-20 22:58:31 -0500

    Create init.lua

---
+++ hammerspoon/init.lua
@@ -0,0 +1,20 @@
+-- A global variable for the Hyper Mode
+hyper = hs.hotkey.modal.new({}, 'F17')
+
+-- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
+function enterHyperMode()
+  hyper.triggered = false
+  hyper:enter()
+end
+
+function exitHyperMode()
+  hyper:exit()
+  if not hyper.triggered then
+    hs.eventtap.keyStroke({}, 'ESCAPE')
+  end
+end
+
+-- Bind the Hyper key
+f18 = hs.hotkey.bind({}, 'F18', enterHyperMode, exitHyperMode)
+
+hs.alert.show("Config loaded")
