da517b3c8441e282f1eaef67def85f5f68bdaa2a
Kenny Baustert <kdbaustert@fullsail.edu>
2020-11-21 16:02:31 -0500

    Update composer.sh

--- setup/composer.sh
+++ setup/composer.sh
@@ -1,13 +1,17 @@
 #! /usr/bin/env sh
 
-
 info "Installing composer global packages."
-composer global require laravel/installer
-composer global require laravel/valet
-composer global require phpunit/phpunit
-composer global require squizlabs/php_codesniffer
-composer global require escapestudios/symfony2-coding-standard
-composer global require dealerdirect/phpcodesniffer-composer-installer
-composer global require wp-coding-standards/wpcs
-composer global require phpcompatibility/phpcompatibility-wp
-composer global require friendsofphp/php-cs-fixer
+composer=(
+  laravel/installer
+  laravel/valet
+  phpunit/phpunit
+  squizlabs/php_codesniffer
+  escapestudios/symfony2-coding-standard
+  dealerdirect/phpcodesniffer-composer-installer
+  wp-coding-standards/wpcs
+  phpcompatibility/phpcompatibility-wp
+  friendsofphp/php-cs-fixer
+)
+
+info ${composer[@]}
+composer global require ${composer[@]}
