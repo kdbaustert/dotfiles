b25b9b1bcf08a56fdab32e3faf4efaac64031a17
Kenny Baustert <kenny@gothamx.dev>
2020-12-09 08:36:41 -0500

    Fixed a link that would break the install

--- install.sh
+++ install.sh
@@ -8,65 +8,13 @@ cd "$DIR"
 . setup/composer.sh
 . setup/npm.sh
 
-info "Prompting for sudo password..."
-if sudo -v; then
-  # Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
-  while true; do
-    sudo -n true
-    sleep 60
-    kill -0 "$$" || exit
-  done 2>/dev/null &
-  success "Sudo credentials updated."
-else
-  error "Failed to obtain sudo credentials."
-fi
-
-info "Installing XCode command line tools..."
-if xcode-select --print-path &>/dev/null; then
-  success "XCode command line tools already installed."
-elif xcode-select --install &>/dev/null; then
-  success "Finished installing XCode command line tools."
-else
-  error "Failed to install XCode command line tools."
-fi
-
-# Check for Homebrew
-if test ! $(which brew); then
-  info 'Installing homebrew...'
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-  info 'Homebrew is already installed!'
-  info 'Updating homebrew && upgrading all formulas'
-  brew update && brew upgrade
-fi
-
-brew update
-
-# Homebrew Fonts
-info 'Intalling brew fonts'
-info ${fonts[@]}
-brew cask install ${fonts[@]}
-
-# Homebrew Binaries
-info 'Intalling brew binaries'
-brew install ${binaries[@]}
-
 # Homebrew Applications
 info 'Intalling brew applications'
 brew install ${apps[@]}
 
-# Composer Global packages
-info "Installing composer global packages."
-info ${composer[@]}
-composer global require ${composer[@]}
-
-# NPM Global packages
-info "Installing npm global packages."
-info ${npm[@]}
-npm install -g ${npm[@]}
-
 # Setup Symlinks
 info "Creating file symlinks."
+ln -s $HOME/dotfiles/.zshrc $HOME/
 ln -s $HOME/dotfiles/skhd/.skhdrc $HOME/
 ln -s $HOME/dotfiles/yabai/.yabairc $HOME/
 ln -s $HOME/dotfiles/spacebar/.spacebarrc $HOME/
@@ -78,7 +26,6 @@ chmod 700 "$HOME/.ssh"
 
 # Gem gobal packages
 gem install colorls
-gem install istats
 
 info "Installing Laravel Valet"
 valet composer
