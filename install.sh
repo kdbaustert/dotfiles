e8a45dc4ab1899dcaa9d6784453fb686053deb83
Kenny Baustert <kenny@gothamx.dev>
2020-12-10 08:53:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,12 +14,12 @@ brew install ${apps[@]}
 
 # Setup Symlinks
 info "Creating file symlinks."
-ln -s $HOME/dotfiles/.zshrc $HOME/
-ln -s $HOME/dotfiles/skhd/.skhdrc $HOME/
-ln -s $HOME/dotfiles/yabai/.yabairc $HOME/
-ln -s $HOME/dotfiles/spacebar/.spacebarrc $HOME/
-ln -s $HOME/dotfiles/neofetch/config.conf $HOME/.config/neofetch
-ln -s $HOME/dotfiles/.mackup.cfg $HOME/
+ln -s "$HOME/dotfiles/.zshrc" $HOME/
+ln -s "$HOME/dotfiles/skhd/.skhdrc" $HOME/
+ln -s "$HOME/dotfiles/yabai/.yabairc" $HOME/
+ln -s "$HOME/dotfiles/spacebar/.spacebarrc" $HOME/
+ln -s "$HOME/dotfiles/neofetch/config.conf" "$HOME/.config/neofetch"
+ln -s "$HOME/dotfiles/.mackup.cfg" $HOME/
 
 info "Setting chmod for ~/.ssh"
 chmod 700 "$HOME/.ssh"
@@ -30,15 +30,18 @@ gem install colorls
 info "Installing Laravel Valet"
 valet composer
 
-mkdir $HOME/Sites
-mkdir $HOME/Dev
+mkdir "$HOME/Sites"
+mkdir "$HOME/Dev"
 
-cd $HOME/Sites
+cd "$HOME/Sites"
 
 valet park
 
-# Setup git
-git config --global user.name "Kenny Baustert"
-git config --global user.email kenny@gothamx.dev
+# Global ignore
+git config --global core.excludesFile "$HOME/.gitignore"
 
-touch ~/.hushlogin
+# Setup git user config
+git config --global user.name "Kenny Baustert"
+git config --global user.email "kenny@gothamx.dev"
+
+touch "$HOME/.hushlogin"
