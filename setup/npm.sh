fc4de8b29a6154cad2d007f9d991ed58da6b02c5
kdbaustert <kenny@gothamx.dev>
2021-03-13 03:59:51 -0500

    Create npm.sh

---
+++ setup/npm.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env sh
+
+npm=(
+  @vue/cli
+  eslint
+  prettier
+  eslint-plugin-prettier
+  eslint-config-prettier
+  eslint-plugin-vue
+  eslint-config-standard
+  eslint-plugin-import
+  eslint-plugin-promise
+  eslint-plugin-node
+  babel-eslint
+  generator-code
+  yo
+  fast-cli
+  fkill-cli
+  vtop
+  vsce
+  @vue/eslint-config-prettier
+  @vue/cli-service-global
+  @vue/eslint-config-prettier
+  cli-error-notifier
+  electron
+  npm-check
+  stylelint
+  stylelint-config-standard
+  alfred-npms
+  electerm
+  firebase-tools
+  ntl
+  prettier-init
+)
