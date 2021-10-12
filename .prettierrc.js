9e95632335a49b909cc6e021586c93d585d7188a
Kenny Baustert <kenny@gothamx.dev>
2021-10-12 10:38:59 -0400

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,17 @@
 module.exports = {
+  useTabs: true,      // Indent lines with tabs instead of spaces.
   printWidth: 80,
+  singleQuote: false,
   trailingComma: 'es5',
   tabWidth: 2,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  proseWrap: 'always',
   arrowParens: 'avoid',
   semi: false,
   singleQuote: true,
   bracketSpacing: true,
+  bracketSameLine: false,
+  quoteProps: 'as-needed',
+  endOfLine: 'lf',
 }
