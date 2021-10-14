c3a1087d07bf940a49011fe5e021c0b71132a331
Kenny Baustert <kenny@gothamx.dev>
2021-10-14 11:01:44 -0400

    Update zinit.zsh

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -12,47 +12,102 @@ autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
 zinit ice depth=1; zinit light romkatv/powerlevel10k
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 # SSH-AGENT
 zinit light bobsoppe/zsh-ssh-agent
 
-zinit ice wait'1' lucid
-zinit light laggardkernel/zsh-gpg-agent
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+zinit ice wait'0a' lucid atload'_zsh_autosuggest_start'
+zinit light zsh-users/zsh-autosuggestions
 
-# zinit ice lucid nocompile wait'0e' nocompletions
-# zinit load MenkeTechnologies/zsh-more-completions
 
-zinit ice wait blockf lucid atpull'zinit creinstall -q .'
-zinit light https://github.com/zsh-users/zsh-completions
+# ENHANCD
+zinit ice wait'0b' lucid
+zinit light b4b4r07/enhancd
+export ENHANCD_FILTER=fzf:fzy:peco
 
-# zinit ice lucid nocompile
-# zinit load MenkeTechnologies/zsh-expand
+# HISTORY SUBSTRING SEARCHING
+zinit ice wait'0b' lucid atload'bindkey "$terminfo[kcuu1]" history-substring-search-up; bindkey "$terminfo[kcud1]" history-substring-search-down'
+zinit light zsh-users/zsh-history-substring-search
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
 
-zinit ice wait lucid atinit'ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay' \
-  atload'unset "FAST_HIGHLIGHT[chroma-whatis]" "FAST_HIGHLIGHT[chroma-man]"'
-zinit light https://github.com/zdharma/fast-syntax-highlighting
-
-zinit ice wait lucid atload'_zsh_autosuggest_start'
-zinit light https://github.com/zsh-users/zsh-autosuggestions
-
-zinit ice atclone"dircolors -b LS_COLORS > clrs.zsh" \
-atpull'%atclone' pick"clrs.zsh" nocompile'!' \
-atload'zstyle ":completion:*" list-colors “${(s.:.)LS_COLORS}”'
-zinit light trapd00r/LS_COLORS
-
-zinit ice wait"!0" blockf lucid pick"init.sh"
-zinit light "b4b4r07/enhancd"
-
-zinit ice from"gh-r" as"program"
-zinit load junegunn/fzf-bin
-
+# TAB COMPLETIONS
+zinit light-mode for \
+    blockf \
+        zsh-users/zsh-completions \
+    as'program' atclone'rm -f ^(rgg|agv)' \
+        lilydjwg/search-and-view \
+    atclone'dircolors -b LS_COLORS > c.zsh' atpull'%atclone' pick'c.zsh' \
+        trapd00r/LS_COLORS \
+    src'etc/git-extras-completion.zsh' \
+        tj/git-extras
 zinit wait'1' lucid for \
     OMZ::lib/clipboard.zsh \
-    OMZ::lib/git.zsh \
-    OMZ::plugins/command-not-found/command-not-found.plugin.zsh \
-    OMZ::plugins/history/history.plugin.zsh \
-    zpm-zsh/ssh \
-    chrissicool/zsh-256color
+    OMZ::lib/git.zsh
+
+if whence dircolors >/dev/null; then
+  eval "$(dircolors -b)"
+  zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+  alias ls='ls --color'
+else
+  export CLICOLOR=1
+  zstyle ':completion:*' list-colors ''
+fi
+
+zstyle ':completion:*' completer _expand _complete _ignored _approximate
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' menu select=2
+zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
+zstyle ':completion:*:descriptions' format '-- %d --'
+zstyle ':completion:*:processes' command 'ps -au$USER'
+zstyle ':completion:complete:*:options' sort false
+zstyle ':fzf-tab:*' query-string prefix first
+# zstyle ':fzf-tab:complete:_zlua:*' query-string input
+zstyle ':fzf-tab:*' continuous-trigger '/'
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm,cmd -w -w"
+zstyle ':fzf-tab:complete:kill:argument-rest' fzf-flags --preview=$extract'ps --pid=$in[(w)1] -o cmd --no-headers -w -w' --preview-window=down:3:wrap
+# zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'  # disable for tmux-popup
+zstyle ':fzf-tab:*' switch-group ',' '.'
+zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+zstyle ':fzf-tab:*' popup-pad 0 0
+zstyle ':completion:*:git-checkout:*' sort false
+zstyle ':completion:*:exa' file-sort modification
+zstyle ':completion:*:exa' sort false
+
+# TMUX plugin manager
+zinit ice lucid wait'!0a' as'null' id-as'tpm' \
+  atclone' \
+    mkdir -p $HOME/.tmux/plugins; \
+    ln -s $HOME/.zinit/plugins/tpm $HOME/.tmux/plugins/tpm; \
+    setup_my_tmux_plugin tpm;'
+zinit light tmux-plugins/tpm
+
+zinit ice lucid wait'0c' as'command' pick'bin/fzf-tmux'
+zinit light junegunn/fzf
+
+# BIND MULTIPLE WIDGETS USING FZF
+zinit ice lucid wait'0c' multisrc'shell/{completion,key-bindings}.zsh' id-as'junegunn/fzf_completions' pick'/dev/null'
+zinit light junegunn/fzf
+
+# FZF-TAB
+
+zinit ice wait'1' lucid
+zinit light Aloxaf/fzf-tab
+
+# SYNTAX HIGHLIGHTING
+zinit ice wait'0c' lucid atinit'zpcompinit;zpcdreplay'
+zinit light zdharma/fast-syntax-highlighting
+
+zinit wait'1' lucid for \
+    OMZP::command-not-found \
+    OMZP::history \
+    OMZP::gpg-agent \
+    chrissicool/zsh-256color \
+    gretzky/auto-color-ls
 
 zinit light xav-b/zsh-extend-history
 
@@ -77,10 +132,9 @@ zinit wait lucid for \
 zinit ice wait"1b" lucid as"program" pick"bin/git-dsf"
 zinit light zdharma/zsh-diff-so-fancy
 
-# fuzzy git
-# https://github.com/wfxr/forgit
-zinit ice has'fzf'
-zinit light wfxr/forgit
+# FORGIT
+zinit ice wait lucid id-as'forgit' atload'alias gr=forgit::checkout::file'
+zinit load 'wfxr/forgit'
 
 # ─── fuzzy movement and directory choosing ────────────────────────────────────
 # autojump command
@@ -90,8 +144,8 @@ zinit light rupa/z
 
 # Pick from most frecent folders with `Ctrl+g`
 # https://github.com/andrewferrier/fzf-z
-: zinit ice wait'0b' lucid
-: zinit load andrewferrier/fzf-z
+# : zinit ice wait'0b' lucid
+# : zinit load andrewferrier/fzf-z
 
 # lets z+[Tab] and zz+[Tab]
 # https://github.com/changyuheng/fz
@@ -101,14 +155,6 @@ zinit light changyuheng/fz
 # sharkdp/fd
 zinit ice as"program" from"gh-r" mv"fd* -> fd" pick"fd/fd"
 zinit light sharkdp/fd
-
-# FZF-TAB
-zinit ice wait'1' lucid
-zinit light Aloxaf/fzf-tab
-
-zinit light-mode for \
-  gretzky/auto-color-ls \
-  MichaelAquilina/zsh-you-should-use \
 
 zinit ice wait'1' lucid
 zinit light laggardkernel/zsh-thefuck
