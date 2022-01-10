01d555358b2e65c7cec4e03c584035bf2e92d549
Kenny B <kenny@kennyb.dev>
2022-01-10 03:09:03 -0500

    Update composer.sh

--- setup/composer.sh
+++ setup/composer.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/env sh
 
 composer=(
-  laravel/installer
-  laravel/valet
-  phpunit/phpunit
-  squizlabs/php_codesniffer
-  escapestudios/symfony2-coding-standard
-  wp-coding-standards/wpcs
-  phpcompatibility/phpcompatibility-wp
-  friendsofphp/php-cs-fixer
-  dealerdirect/phpcodesniffer-composer-installer
+	laravel/installer
+	laravel/valet
+	phpunit/phpunit
+	squizlabs/php_codesniffer
+	escapestudios/symfony2-coding-standard
+	wp-coding-standards/wpcs
+	phpcompatibility/phpcompatibility-wp
+	friendsofphp/php-cs-fixer
+	wptrt/wpthemereview
+	dealerdirect/phpcodesniffer-composer-installer
 )
