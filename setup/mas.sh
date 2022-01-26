a83bcdc9e452d4ecc8cbadbbe05dc74858e6ed7e
Kenny Baustert <kenny@gothamx.dev>
2022-01-26 09:00:43 -0500

    Create mas.sh

---
+++ setup/mas.sh
@@ -0,0 +1,8 @@
+mas install 497799835  # Xcode
+mas install 975937182  # Fantastical
+mas install 1507782672 # Pixea
+mas install 1355679052 # Dropover
+mas install 1456386228 # Clockology
+mas install 1436522307 # Transmit 5
+mas install 937984704  # Amphetamine
+mas install 1176895641 # Spark
