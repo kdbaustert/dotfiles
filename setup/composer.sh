cb9db39fc80db9ac158e83d3dc2f267b3127cce0
Kenny Baustert <kenny@kennyb.dev>
2022-05-03 08:29:01 -0400

    Update composer.sh

--- setup/composer.sh
+++ setup/composer.sh
@@ -1,19 +1,20 @@
 #! /usr/bin/env sh
 
 composer=(
-  laravel/installer
-  laravel/valet
-  phpunit/phpunit
-  friendsofphp/php-cs-fixer
-  squizlabs/php_codesniffer
-  automattic/phpcs-neutron-standard
-  automattic/phpcs-neutron-ruleset
-  dealerdirect/phpcodesniffer-composer-installer
-  phpmd/phpmd
-  phpstan/phpstan
-  szepeviktor/phpstan-wordpress
-  php-stubs/woocommerce-stubs
-  wp-coding-standards/wpcs
-  wptrt/wpthemereview
-  woocommerce/woocommerce-sniffs
+	laravel/installer
+	laravel/valet
+	spatie/global-ray
+	phpunit/phpunit
+	friendsofphp/php-cs-fixer
+	squizlabs/php_codesniffer
+	automattic/phpcs-neutron-standard
+	automattic/phpcs-neutron-ruleset
+	dealerdirect/phpcodesniffer-composer-installer
+	phpmd/phpmd
+	phpstan/phpstan
+	szepeviktor/phpstan-wordpress
+	php-stubs/woocommerce-stubs
+	wp-coding-standards/wpcs
+	wptrt/wpthemereview
+	woocommerce/woocommerce-sniffs
 )
