9ac88a8d6e034b3081ac5fbbb56be15284f15472
Kenny Baustert <kenny@kennyb.dev>
2022-08-28 18:57:10 -0400

    Removed sketchybar

--- .config/sketchybar/plugins/window_title.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-APP_NAME=$(yabai -m query --windows --window | jq '.app' | sed -E 's/^"(.*)"$/\1/')
-
-if [ "$APP_NAME" = "" ]; then
-  sketchybar --set $NAME label="Finder"
-else 
-  sketchybar --set $NAME label="$APP_NAME"
-fi
