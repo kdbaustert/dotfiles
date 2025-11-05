c1c29470ca2a87c2ac02b7fbd47e651bd4d267e8
Kenny B <kenny@kennyb.dev>
2025-11-05 08:40:50 -0500

    Update zinit.zsh

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -2,16 +2,17 @@
 # ## Setup zinit                                                ##
 # #==============================================================#
 
-if [[ ! -f $HOME/.zi/bin/zi.zsh ]]; then
+if [[ ! -f $HOME/.zi/bin/zinit.zsh ]]; then
   print -P "%F{33}▓▒░ %F{160}Installing (%F{33}z-shell/zi%F{160})…%f"
   command mkdir -p "$HOME/.zi" && command chmod g-rwX "$HOME/.zi"
-  command git clone -q --depth=1 --branch "main" https://github.com/z-shell/zi "$HOME/.zi/bin" && \
-    print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
-    print -P "%F{160}▓▒░ The clone has failed.%f%b"
+  command git clone -q --depth=1 --branch "main" https://github.com/zdharma-continuum/zinit "$HOME/.zi/bin" && \
+  print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+  print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
 
-source "$HOME/.zi/bin/zi.zsh"
+source "$HOME/.zi/bin/zinit.zsh"
 autoload -Uz _zi
+
 (( ${+_comps} )) && _comps[zi]=_zi
 
 declare -A _bpicks=()
@@ -24,28 +25,26 @@ if [[ $OSTYPE == darwin* ]] && [[ $CPUTYPE == arm* ]]; then
   _bpicks[starship]="*aarch64-apple*.tar.gz"
 fi
 
-zi light-mode for z-shell/z-a-meta-plugins @annexes @ext-git z-shell/H-S-MW
-
 zi lucid light-mode for \
   as"command" from"gh-r" atload'eval "$(starship init zsh)"' bpick="*aarch64-apple*.tar.gz" \
   starship/starship
 
-setopt promptsubst
+setopt PROMPT_SUBST
 
 zi ice lucid wait="0" pick="asdf.sh"
 zi light $HOME/.asdf
 
 # zi snippet OMZ::lib/key-bindings.zsh
 
-zi from"gh-r" as"null" for \
-  sbin"**/fd" @sharkdp/fd \
-  sbin"**/bat" @sharkdp/bat \
-  sbin"**/bat" @sharkdp/bat \
-  sbin"**/exa -> exa" atclone"cp -vf completions/exa.zsh _exa" ogham/exa
+# zi from"gh-r" as"null" for \
+#   sbin"**/fd" @sharkdp/fd \
+#   sbin"**/bat" @sharkdp/bat \
+#   sbin"**/bat" @sharkdp/bat \
+#   sbin"**/exa -> exa" atclone"cp -vf completions/exa.zsh _exa" ogham/exa
 
-zi light-mode for pick'misc/quitcd/quitcd.zsh' as'program' nocompile \
-  sbin make \
-    jarun/nnn`  `
+# zi light-mode for pick'misc/quitcd/quitcd.zsh' as'program' nocompile \
+#   sbin make \
+#     jarun/nnn`  `
 
 zi ice lucid wait has'fzf'
 zi light Aloxaf/fzf-tab
@@ -59,8 +58,11 @@ zi light b4b4r07/enhancd
 zi ice lucid wait as'completion'
 zi light zsh-users/zsh-completions
 
-zi ice wait lucid as'program' from'gh-r' sbin'**/delta -> delta'
-zi light dandavison/delta
+zinit ice depth=1
+zinit load zsh-users/zsh-completions
+
+# zi ice wait lucid as'program' from'gh-r' sbin'**/delta -> delta'
+# zi light dandavison/delta
 
 zi ice lucid wait as'program' from"gh-r" has'fzf'
 zi light denisidoro/navi
@@ -75,31 +77,39 @@ zi ice eval"dircolors -b LS_COLORS" \
   atload'zstyle ":completion:*" list-colors “${(s.:.)LS_COLORS}”'
 zi light trapd00r/LS_COLORS
 
-zi light-mode for id-as'pnpm' from'gh-r' bpick'*-linux-x64' as'program' \
-  atinit'export PNPM_HOME=$ZPFX/bin; [[ -z $NODE_PATH ]] && \
-  export NODE_PATH=$PWD' sbin'pnpm* -> pnpm' nocompile \
-    pnpm/pnpm
+# zi light-mode for id-as'pnpm' from'gh-r' bpick'*-linux-x64' as'program' \
+#   atinit'export PNPM_HOME=$ZPFX/bin; [[ -z $NODE_PATH ]] && \
+#   export NODE_PATH=$PWD' sbin'pnpm* -> pnpm' nocompile \
+#     pnpm/pnpm
 
 zi lucid as'command' pick'bin/pyenv' atinit'export PYENV_ROOT="$PWD"' \
     atclone'PYENV_ROOT="$PWD" ./libexec/pyenv init - > zpyenv.zsh' \
     atpull"%atclone" src"zpyenv.zsh" nocompile'!' for \
         pyenv/pyenv
 
-zi wait lucid for \
- atinit"ZI[COMPINIT_OPTS]=-C; zpcompinit; zicompinit; zicdreplay" \
-  z-shell/fast-syntax-highlighting \
+zinit wait lucid for \
+ atinit"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zicompinit; zicdreplay" \
+    zdharma-continuum/fast-syntax-highlighting \
  blockf \
     zsh-users/zsh-completions \
  atload"!_zsh_autosuggest_start" \
-  zsh-users/zsh-autosuggestions
+    zsh-users/zsh-autosuggestions
+
+# Enable compinit
+autoload -U compinit && compinit
+
+# Optionally load other plugins here (autosuggestions, history search, etc.)
+zinit load zsh-users/zsh-autosuggestions
+
+# Load fast-syntax-highlighting LAST
+zinit load zdharma-continuum/fast-syntax-highlighting
+zinit load z-shell/fast-syntax-highlighting
 
 zi ice lucid wait as'completion' blockf pick'src/go' src'src/zsh'
 zi light zchee/zsh-completions
 
-ZSH_AUTOSUGGEST_USE_ASYNC=true
-
-# zi ice wait lucid
-# zi snippet "OMZ::lib/completion.zsh"
+zi ice wait lucid
+zi snippet "OMZ::lib/completion.zsh"
 
 zi wait lucid for \
   has'exa' atinit'AUTOCD=1' \
@@ -119,22 +129,21 @@ zi ice wait lucid reset \
  atload'zstyle ":completion:*:default" list-colors "${(s.:.)LS_COLORS}";'
 zi light trapd00r/LS_COLORS
 
-zi light-mode for from'gh-r' as'program' \
-  atinit'export PATH="$HOME/.yarn/bin:$PATH"' mv'yarn* -> yarn' \
-  pick"yarn/bin/yarn" bpick'*.tar.gz' \
-    yarnpkg/yarn
+# zi light-mode for from'gh-r' as'program' \
+#   atinit'export PATH="$HOME/.yarn/bin:$PATH"' mv'yarn* -> yarn' \
+#   pick"yarn/bin/yarn" bpick'*.tar.gz' \
+#     yarnpkg/yarn
 
 # zi ice wait"0" lucid
 # zi light htlsne/zinit-rbenv
 
-export NVM_LAZY_LOAD=true
 zi light lukechilds/zsh-nvm
 
 zi ice lucid wait"0a" from"gh-r" as"program" atload'eval "$(mcfly init zsh)"'
 zi light cantino/mcfly
 
 myfzf () {
-        local bindir="/home/sg/.zi/plugins/junegunn---fzf"
-        local -x GEM_HOME="/home/sg/.zi/plugins/junegunn---fzf"
-        "$bindir"/"fzf" "$@"
+	local bindir="/home/sg/.zi/plugins/junegunn---fzf"
+	local -x GEM_HOME="/home/sg/.zi/plugins/junegunn---fzf"
+	"$bindir"/"fzf" "$@"
 }
